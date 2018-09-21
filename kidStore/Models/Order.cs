f5ab9f31bf7574ea4b46ad3256b498bf5a17e6e0
Veronika <veronikazolobov@gmail.com>
2018-09-21 03:14:25 +0300

    add banners

--- kidStore/Models/Order.cs
+++ kidStore/Models/Order.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace kidStore.Models
 {
     public class Order
     {
+        public int orderId { get; set; }
+        public int userId { get; set; }
+        public string address { get; set; }
+        public decimal total { get; set; }
+        public System.DateTime orderDate { get; set; }
+        public List<ProductOrder> productsInOrder { get; set; }
     }
 }
